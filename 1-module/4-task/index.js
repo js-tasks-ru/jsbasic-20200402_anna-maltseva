980665df144901412af644e3b2f31ca1445065df
Anna Maltseva <Annnushka8500@gmail.com>
2020-04-19 20:11:56 +0300

    1 урок 4 и 5 задачи

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,14 @@
  * @param {string} str base
  * @returns {boolean}
  */
-function checkSpam(str) {
-  // ваш код...
+function checkSpam(str){
+  let lowerStr = str.toLowerCase();
+
+ return lowerStr.includes('1xbet') || lowerStr.includes('xxx');
+   
 }
+
+console.log( checkSpam('1XbeT now') );
+console.log( checkSpam('free xxxxx') );
+console.log( checkSpam('innocent rabbit') );
+
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,8 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  // ваш код...
+  return (str.length > maxlength) ?
+    str.slice(0, maxlength - 1) + '…' : str;
 }
+console.log(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20));
+console.log(truncate('Всем привет!', 20));
